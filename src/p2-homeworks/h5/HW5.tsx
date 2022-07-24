6beeee5767e977b3f780f4f83744915ba014ed51
Nazaruk-D <nazaruk-dima@mail.ru>
2022-07-24 13:52:10 +0300

    HW5

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Header from './Header'
+import React, {useState} from 'react'
+import {Header} from './Header'
 import Routes from './Routes'
 import {HashRouter} from "react-router-dom";
 
 function HW5() {
+
+const[menuCollapsed, setMenuCollapsed] = useState(false)
+
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
             <HashRouter>
-                <Header/>
+                <Header onClick={()=>setMenuCollapsed(!menuCollapsed)}
+                        collapsed={menuCollapsed}
+                        />
                 <Routes/>
             </HashRouter>
         </div>
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,23 +3,50 @@ import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes";
 import s from "./Header.module.css"
 
-function Header() {
+
+export type HeaderPropsType = {
+    onClick: () => void
+    collapsed: boolean
+}
+
+
+export const Header: React.FC<HeaderPropsType> = ({onClick, collapsed}) => {
+    const inputClass = collapsed ? s.navbarActive : s.navbar;
+
     return (
-        <div className={s.navbar}>
-            <div className={s.block}>
-                <NavLink to={PATH.PRE_JUNIOR} className={s.navText}>PreJunior</NavLink>
-            </div >
-            <div className={s.block}>
-                <NavLink to={PATH.JUNIOR} className={s.navText}>Junior</NavLink>
-            </div>
-            <div className={s.block}>
-                <NavLink to={PATH.JUNIOR_PLUS} className={s.navText}>JuniorPlus</NavLink>
-            </div>
-
-
-
+        <div className={inputClass}>
+            <MenuActivator onClick={onClick}/>
+            {collapsed && <Menu/>}
         </div>
     )
 }
 
-export default Header
+export const Menu = () => {
+    return (<>
+            <span className={s.block}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.navText} activeClassName={s.active}>PreJunior</NavLink>
+            </span>
+            <span className={s.block}>
+                <NavLink to={PATH.JUNIOR} className={s.navText} activeClassName={s.active}>Junior</NavLink>
+            </span>
+            <span className={s.block}>
+                <NavLink to={PATH.JUNIOR_PLUS} className={s.navText} activeClassName={s.active}>JuniorPlus</NavLink>
+            </span>
+        </>
+    )
+}
+
+
+export type MenuActivatorPropsType = {
+    onClick: () => void
+}
+
+const MenuActivator: React.FC<MenuActivatorPropsType> = ({onClick}) => {
+
+    return (
+        <span className={s.block} onClick={e => onClick()} ><span className={s.menuActivator}>Menu</span></span>
+    )
+}
+
+
+
