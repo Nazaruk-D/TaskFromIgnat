6d624bbec1a7836f840da3b5f8d779e7b54d5431
Nazaruk-D <nazaruk-dima@mail.ru>
2022-07-24 14:17:28 +0300

    Update HM5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import s from "./Error404.module.css"
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-
+        <div className={s.img404}>
+            <img src="https://static.vecteezy.com/system/resources/previews/003/393/235/original/error-404-with-the-cute-floppy-disk-mascot-free-vector.jpg" alt=""  className={s.img404}/>
         </div>
     )
 }
